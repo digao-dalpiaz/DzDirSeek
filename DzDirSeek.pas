46636623bab4afeffc5be87e25bab2009b7a7eeb
Rodrigo Depiné Dalpiaz <46692097+digao-dalpiaz@users.noreply.github.com>
2019-02-23 21:42:43 -0300

    Initial commit

---
+++ DzDirSeek.pas
@@ -0,0 +1,247 @@
+{------------------------------------------------------------------------------
+TDzDirSeek component
+Developed by Rodrigo Depiné Dalpiaz (digão dalpiaz)
+Non visual component to search files in directories
+
+https://github.com/digao-dalpiaz/DzDirSeek
+
+Please, read the documentation at GitHub link.
+------------------------------------------------------------------------------}
+
+unit DzDirSeek;
+
+interface
+
+uses System.Classes;
+
+type
+  TDSResultKind = (rkComplete, rkRelative, rkOnlyName);
+  TDSMaskKind = (mkExceptions, mkInclusions);
+
+  TDzDirSeek = class(TComponent)
+  private
+    FDir: String;
+    FSubDir: Boolean;
+    FSorted: Boolean;
+    FResultKind: TDSResultKind;
+    FMaskKind: TDSMaskKind;
+    FUseMask: Boolean;
+    FMasks: TStrings;
+
+    FList: TStringList;
+
+    BaseDir: String;
+    procedure IntSeek(const RelativeDir: String);
+    function CheckMask(const aFile: String; IsDir: Boolean): Boolean;
+    function GetName(const RelativeDir, Nome: String): String;
+    procedure DoSort;
+  public
+    constructor Create(AOwner: TComponent); override;
+    destructor Destroy; override;
+
+    procedure Seek;
+
+    property List: TStringList read FList;
+  published
+    property Dir: String read FDir write FDir;
+    property SubDir: Boolean read FSubDir write FSubDir default True;
+    property Sorted: Boolean read FSorted write FSorted default False;
+    property ResultKind: TDSResultKind read FResultKind write FResultKind default rkComplete;
+    property MaskKind: TDSMaskKind read FMaskKind write FMaskKind default mkExceptions;
+    property UseMask: Boolean read FUseMask write FUseMask default True;
+    property Masks: TStrings read FMasks write FMasks;
+  end;
+
+function BytesToMB(X: Int64): String;
+function GetFileSize(const aFileName: String): Int64;
+
+procedure Register;
+
+implementation
+
+uses System.SysUtils, System.Masks, System.StrUtils;
+
+procedure Register;
+begin
+  RegisterComponents('Digao', [TDzDirSeek]);
+end;
+
+//
+
+constructor TDzDirSeek.Create(AOwner: TComponent);
+begin
+  inherited;
+
+  FSubDir := True;
+  FResultKind := rkComplete;
+  FMaskKind := mkExceptions;
+  FUseMask := True;
+  FMasks := TStringList.Create;
+  FList := TStringList.Create;
+end;
+
+destructor TDzDirSeek.Destroy;
+begin
+  FMasks.Free;
+  FList.Free;
+
+  inherited;
+end;
+
+procedure TDzDirSeek.Seek;
+begin
+  if not DirectoryExists(FDir) then
+    raise Exception.CreateFmt('Path "%s" not found', [FDir]);
+
+  BaseDir := IncludeTrailingPathDelimiter(FDir);
+
+  FList.Clear;
+  IntSeek('');
+
+  if FSorted then DoSort;
+end;
+
+procedure TDzDirSeek.IntSeek(const RelativeDir: String);
+var Sr: TSearchRec;
+
+  function IntCheckMask(IsDir: Boolean): Boolean;
+  begin
+    Result := CheckMask(RelativeDir + Sr.Name, IsDir);
+  end;
+
+begin
+  if FindFirst(BaseDir + RelativeDir + '*', faAnyFile, Sr) = 0 then
+  begin
+    repeat
+      if (Sr.Name = '.') or (Sr.Name = '..') then Continue;
+
+      if (Sr.Attr and faDirectory) <> 0 then
+      begin //directory
+        if FSubDir then //include sub-directories
+        begin
+          if IntCheckMask(True{Dir}) then
+            IntSeek(RelativeDir + Sr.Name + '\');
+        end;
+      end else
+      begin //file
+        if IntCheckMask(False) then
+          FList.Add(GetName(RelativeDir, Sr.Name));
+      end;
+
+    until FindNext(Sr) <> 0;
+    FindClose(Sr);
+  end;
+end;
+
+function TDzDirSeek.CheckMask(const aFile: String; IsDir: Boolean): Boolean;
+
+type
+  TProps = (pOnlyFile);
+  TPropsSet = set of TProps;
+
+  function GetProps(var Mask: String): TPropsSet;
+  var Props: TPropsSet;
+
+    procedure CheckProp(const aProp: String; pProp: TProps);
+    var aIntProp: String;
+    begin
+      aIntProp := '<'+aProp+'>';
+    
+      if Mask.Contains(aIntProp) then
+      begin
+        Include(Props, pProp);
+        Mask := StringReplace(Mask, aIntProp, '', []); //you should type parameter just once!
+      end;
+    end;
+
+  begin
+    Props := [];
+
+    CheckProp('F', pOnlyFile); //only file parameter
+      
+    Result := Props;
+  end;
+
+var
+  aPreMask, aMask: String;
+  P: TPropsSet;
+  Normal: Boolean; //not OnlyFile
+begin
+  Result := False;
+  if not FUseMask then Exit(True);
+  if IsDir and (FMaskKind=mkInclusions) then Exit(True);
+
+  for aPreMask in FMasks do
+  begin
+    aMask := aPreMask;
+    P := GetProps(aMask);
+
+    Normal := not (pOnlyFile in P); //not OnlyFile
+
+    if ( Normal and MatchesMask(aFile, aMask) )
+    or ( (Normal or not IsDir) and MatchesMask(ExtractFileName(aFile), aMask) ) then
+    begin
+      Result := True;
+      Break;
+    end;
+  end;
+
+  if FMaskKind = mkExceptions then Result := not Result;
+end;
+
+function TDzDirSeek.GetName(const RelativeDir, Nome: String): String;
+begin
+  case FResultKind of
+    rkComplete: Result := BaseDir + RelativeDir + Nome;
+    rkRelative: Result := RelativeDir + Nome;
+    rkOnlyName: Result := Nome;
+  end;
+end;
+
+// ============================================================================
+
+function SortItem(List: TStringList; Index1, Index2: Integer): Integer;
+var A1, A2: String;
+    Dir1, Dir2: String;
+    Name1, Name2: String;
+begin
+  A1 := List[Index1];
+  A2 := List[Index2];
+
+  Dir1 := ExtractFilePath(A1);
+  Dir2 := ExtractFilePath(A2);
+
+  Name1 := ExtractFileName(A1);
+  Name2 := ExtractFileName(A2);
+
+  if Dir1 = Dir2 then
+    Result := AnsiCompareText(Name1, Name2)
+  else
+    Result := AnsiCompareText(Dir1, Dir2);
+end;
+
+procedure TDzDirSeek.DoSort;
+begin
+  FList.CustomSort(SortItem);
+end;
+
+// ============================================================================
+
+function BytesToMB(X: Int64): String;
+begin
+  Result := FormatFloat('0.00 MB', X / 1024 / 1024);
+end;
+
+function GetFileSize(const aFileName: String): Int64;
+var Sr: TSearchRec;
+begin
+  if FindFirst(aFileName, faAnyFile, Sr) = 0 then
+  begin
+    Result := Sr.Size;
+
+    FindClose(Sr);
+  end
+    else raise Exception.CreateFmt('Could not get file size of "%s"', [aFileName]);
+end;
+
+end.
---
+++ README.md
@@ -0,0 +1,77 @@
+# DzDirSeek
+
+## Delphi non-visual component to search files in directories
+
+![Delphi Supported Versions](https://img.shields.io/badge/Delphi%20Supported%20Versions-XE2..10.3%20Rio-blue.svg)
+![Platforms](https://img.shields.io/badge/Platforms-Win32%20and%20Win64-red.svg)
+![Auto Install](https://img.shields.io/badge/-Auto%20Install%20App-orange.svg)
+
+## Introduction
+
+When using Delphi and working with files and directories, eventually you need to search and get a list of files in a directory, or even search files in sub-directories.
+
+This could be a little difficult using old `FindFirst` and `FindNext` functions. Now we have new methods overloads `TDirectory.GetFiles`, available at `System.IOUtils` unit.
+
+But, even using GetFiles methods, if you need some advanced parameters like include sub-directories, you will need to iterate all directories. Also if you want to specify mask that include some part of path string, you will need to work with strings in your code every time you need this resource.
+
+So, I decided to build this component to become an easy way to search files.
+
+## Installing
+
+### Auto install
+
+Close Delphi IDE and run **CompInstall.exe** app to auto install component into Delphi.
+
+### Manual install
+
+1. Open **DzDirSeek** package in Delphi.
+2. Ensure **Win32** Platform and **Release** config are selected.
+3. Then **Build** and **Install**.
+4. If you want to use Win64 platform, select this platform and Build again.
+5. Add sub-path Win32\Release to the Library paths at Tools\Options using 32-bit option, and if you have compiled to 64 bit platform, add sub-path Win64\Release using 64-bit option.
+
+Supports Delphi XE2..Delphi 10.3 Rio
+
+## How to use
+
+Just fill desired properties and call method `Seek`.
+
+Then you can read the public property `List` (TStringList) to get all found files.
+
+## Properties
+
+`Dir: String` = Path to search
+
+`MaskKind: TDSMaskKind` =
+
+- mkInclusions: Only the masks specifyed at **Masks** property will be included in results. *If you leave Masks propery blank, no file will be retrieved.*
+
+- mkExceptions (default): All files will be included in results, except the files which matches **Masks** properties. *If you leave Masks property blank, all files will be retrieved.*
+
+`Masks: TStrings` = The list of masks using Windows Masks typing, each mask per line. Some allowed masks:
+
+```
+*.txt
+myapp.exe
+*xyz*.doc
+myfile?.rtf
+*\sub_path\*
+first_path\*
+path1\path2\file.avi
+```
+
+*This property depends on **UseMask** property enabled. Also it will work according to **MaskKing** property definition.*
+
+`ResultKind: TDSResultKind` = 
+
+- rkComplete (default): The result will include the full file path (search path + sub-directories + file name)
+
+- rkRelative: The result will include only the relative path, without the search path (sub-directories + file name)
+
+- rkOnlyName: The result will include only the file name, without search path or sub-directories.
+
+`Sorted: Boolean` = If enabled, it will retrieve directories and files alphabetically sorted. (default False)
+
+`SubDir: Boolean` = If enabled, it will scan files in all sub-directories inside search path. (default True)
+
+`UseMask: Boolean` = If enabled, it will consider **Masks** and **MaskKind** properties. If disabled, it will retrieve always all files. (default Enabled).
