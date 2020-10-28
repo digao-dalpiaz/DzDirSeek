fa9fb6c2ca1e3e4e7253753ca56a9d64c620e8c4
Rodrigo Depiné Dalpiaz <dalpiaze@gmail.com>
2020-10-27 21:39:08 -0300

    Delphi versions compatibility

--- DzDirSeek.pas
+++ DzDirSeek.pas
@@ -76,7 +76,7 @@ constructor TDzDirSeek.Create(AOwner: TComponent);
 begin
   inherited;
 
-  FAbout := 'Digao Dalpiaz / Version 1.1';
+  FAbout := 'Digao Dalpiaz / Version 1.2';
 
   FSubDir := True;
   FResultKind := rkComplete;
